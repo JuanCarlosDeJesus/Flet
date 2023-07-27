0a994d5008ed73fa5df7f2c59315f879accb9f10
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2023-07-27 14:14:02 +0800

    first grammerly.py commit

---
+++ grammerly.py
@@ -0,0 +1,16 @@
+''' Fler Grammerly App '''
+import flet as ft 
+
+
+
+def main(page: ft.Page):
+    ''' main function '''
+    page.title = "Grammerly App"
+    
+    
+    
+    page.add()
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
