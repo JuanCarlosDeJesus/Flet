dc72b24609bc874d20539af140ef45b78e619b8c
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2023-07-28 15:51:00 +0800

    grammerly.py second edit

--- grammerly.py
+++ grammerly.py
@@ -6,11 +6,21 @@ import flet as ft
 def main(page: ft.Page):
     ''' main function '''
     page.title = "Grammerly App"
+    page.horizontal_alignment = "center"
+    page.theme_mode = "light"
+    page.window_max_height = 600
+    page.window_max_width = 700
+    page.window_height = 600
+    page.window_width = 700
+    
+    logo = ft.Image(src=f"logo.jpg", width=300)
     
     
     
-    page.add()
+    page.add(
+        logo
+    )
 
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, assets_dir="assets")
